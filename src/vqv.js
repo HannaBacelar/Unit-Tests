2be8eb853a81cdbff3aab55a9be1ca8abc900b9a
Hanna Bacelar <hannabacelar@hotmail.com>
2022-01-18 00:42:01 -0300

    requisito 3, concertado

--- src/vqv.js
+++ src/vqv.js
@@ -20,10 +20,9 @@ const vqv = (nome, idade) => {
  if (typeof nome !== 'string' || typeof idade !== 'number') {
    return undefined;
  } 
- return (`Oi, meu nome é ${nome}! 
- Tenho ${idade} anos,
- trabalho na Trybe e mando muito em programação!
- #VQV!`);
+  return (`Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`); 
 };
-
 module.exports = vqv;
